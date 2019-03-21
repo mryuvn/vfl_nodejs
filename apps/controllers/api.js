f9522ba678f9d20b6d77e2ea49be0dfe7f1bd30d
Cao Ngoc Anh <ngocanh.vietnamtour@gmail.com>
2019-03-21 21:21:36 +0700

    update api

--- apps/controllers/api.js
+++ apps/controllers/api.js
@@ -11,7 +11,7 @@ var db_demo_model = require("../models/db_demo_models");
 var countries_data = require('../common/countries_data');
 
 router.get("/", function (req, res) {
-    res.json({"mess": "This is APIs page"});
+    res.json({ "mess": "This is APIs page" });
 });
 
 router.get("/get-data", (req, res) => {
@@ -115,30 +115,30 @@ router.post("/add-data", jsonParser, (req, res) => {
                     fields.num = data.length + 1;
                     // res.json({fields: fields});
                     db_model.addData(data_table, fields)
-                        .then(result => res.json({ 
-                            "mess": "ok", 
-                            "result": result, 
+                        .then(result => res.json({
+                            "mess": "ok",
+                            "result": result,
                             "code": fields.code,
                             "reference": fields.reference,
                             "year": fields.year,
                             "month": fields.month,
                             "date": fields.date,
                             "num": fields.num,
-                            "createdTime": fields.createdTime 
+                            "createdTime": fields.createdTime
                         }))
                         .catch(err => res.json({ "mess": "fail", "err": err }));
                 }).catch(err => res.json({ "mess": "fail", "err": err }));
         } else {
             db_model.addData(data_table, fields)
                 .then(result => res.json({
-                    "mess": "ok", 
-                    "result": result, 
+                    "mess": "ok",
+                    "result": result,
                     "code": fields.code,
                     "reference": fields.reference,
                     "year": fields.year,
                     "month": fields.month,
                     "date": fields.date,
-                    "createdTime": fields.createdTime 
+                    "createdTime": fields.createdTime
                 }))
                 .catch(err => res.json({ "mess": "fail", "err": err }));
         }
@@ -226,10 +226,10 @@ router.get("/get-currency-data", (req, res) => {
                     currenciesData.unshift(vndData);
                     res.json({ "mess": "ok", "data": currenciesData });
                 } else {
-                    res.json({"mess": "fail", "err": "noContent"});
+                    res.json({ "mess": "fail", "err": "noContent" });
                 }
             } else {
-                res.json({"mess": "fail", "err": "dataNotFound"});
+                res.json({ "mess": "fail", "err": "dataNotFound" });
             }
         })
         .catch(err => res.json({ "mess": "fail", "err": err }));
@@ -278,7 +278,7 @@ router.get("/get-site-values", (req, res) => {
                 } else {
                     res.json({ "mess": "noData" });
                 }
-    
+
             }).catch(err => res.json({ "mess": "fail", "err": err }));
     } else {
         res.json({
@@ -291,48 +291,191 @@ router.get("/get-site-values", (req, res) => {
 //database: vflco_demo
 router.get("/db-demo/get-data", (req, res) => {
     var secur_key = req.query.secur_key;
-    if (secur_key) {
-        if (secur_key == api_secur.db_demo_secur) {
-            var db = req.query.db;
-            if (db) {
-                if (req.query.fields) {
-                    var fields = req.query.fields;
-                } else {
-                    var fields = '*';
-                }
-                if (req.query.where) {
-                    var where = req.query.where;
-                } else {
-                    var where = '';
-                }
-                if (req.query.orderBy) {
-                    var orderBy = req.query.orderBy;
-                } else {
-                    var orderBy = '';
-                }
-                if (req.query.limit) {
-                    var limit = req.query.limit;
-                } else {
-                    var limit = '';
-                }
-                db_demo_model.getData(db, fields, where, orderBy, limit)
-                    .then(data => {
-                        res.json({ "mess": "ok", "data": data });
-                    })
-                    .catch(err => res.json({ "mess": "fail", "err": err }));
+    if (secur_key === api_secur.db_demo_secur) {
+        var db = req.query.db;
+        if (db) {
+            if (req.query.fields) {
+                var fields = req.query.fields;
             } else {
-                res.json({ "mess": "fail", "err": "No dataTable!" });
+                var fields = '*';
+            }
+            if (req.query.where) {
+                var where = req.query.where;
+            } else {
+                var where = '';
+            }
+            if (req.query.orderBy) {
+                var orderBy = req.query.orderBy;
+            } else {
+                var orderBy = '';
+            }
+            if (req.query.limit) {
+                var limit = req.query.limit;
+            } else {
+                var limit = '';
+            }
+            db_demo_model.getData(db, fields, where, orderBy, limit)
+                .then(data => {
+                    res.json({ "mess": "ok", "data": data });
+                })
+                .catch(err => res.json({ "mess": "fail", "err": err }));
+        } else {
+            res.json({ "mess": "fail", "err": "No dataTable!" });
+        }
+    } else {
+        res.json({
+            "mess": "fail",
+            "err": "Please privide the correct API Security key!"
+        });
+    }
+});
+
+router.post("/db-demo/add-data", jsonParser, (req, res) => {
+    if (req.body) {
+        var secur_key = req.body.secur_key;
+        if (secur_key === api_secur.db_demo_secur) {
+            var data_table = req.body.data_table;
+            var fields = req.body.fields;
+
+            if (req.body.options) {
+                var options = req.body.options;
+            } else {
+                var options = {}
+            }
+
+            if (options.setCode) {
+                fields.code = func.randomString(options.setCode.length, options.setCode.charset, options.setCode.capitalization);
+            }
+
+            if (options.setReference) {
+                fields.reference = func.randomString(options.setReference.length, options.setReference.charset, options.setReference.capitalization);
+            }
+
+            var time = new Date();
+            var vnTime = time.toLocaleString('en-US', { timeZone: 'Asia/Ho_Chi_Minh' });
+            var thisTime = new Date(vnTime);
+            var year = thisTime.getFullYear();
+            var month = thisTime.getMonth() + 1;
+            var date = thisTime.getDate();
+
+            if (options.setTime) {
+                fields.createdTime = thisTime;
+            }
+
+            if (options.setYear) {
+                fields.year = year;
+            }
+            if (options.setMonth) {
+                fields.month = month;
+            }
+            if (options.setDate) {
+                fields.date = date;
+            }
+
+            if (options.setNum) {
+                if (options.setNum == 'year') {
+                    var where = 'WHERE year = "' + year + '"';
+                } else if (options.setNum == 'month') {
+                    var where = 'WHERE month = "' + month + '"';
+                } else {
+                    var where = 'WHERE date = "' + date + '"';
+                }
+                db_demo_model.getData(data_table, 'id', where, '', '')
+                    .then(data => {
+                        fields.num = data.length + 1;
+                        // res.json({fields: fields});
+                        db_model.addData(data_table, fields)
+                            .then(result => res.json({
+                                "mess": "ok",
+                                "result": result,
+                                "code": fields.code,
+                                "reference": fields.reference,
+                                "year": fields.year,
+                                "month": fields.month,
+                                "date": fields.date,
+                                "num": fields.num,
+                                "createdTime": fields.createdTime
+                            }))
+                            .catch(err => res.json({ "mess": "fail", "err": err }));
+                    }).catch(err => res.json({ "mess": "fail", "err": err }));
+            } else {
+                db_demo_model.addData(data_table, fields)
+                    .then(result => res.json({
+                        "mess": "ok",
+                        "result": result,
+                        "code": fields.code,
+                        "reference": fields.reference,
+                        "year": fields.year,
+                        "month": fields.month,
+                        "date": fields.date,
+                        "createdTime": fields.createdTime
+                    }))
+                    .catch(err => res.json({ "mess": "fail", "err": err }));
             }
         } else {
             res.json({
                 "mess": "fail",
-                "err": "Security key is not right!"
+                "err": "Please privide the correct API Security key!"
             });
         }
     } else {
         res.json({
             "mess": "fail",
-            "err": "No security key!"
+            "err": "No data post received!"
+        });
+    }
+});
+
+router.post("/edit-data", jsonParser, (req, res) => {
+    if (req.body) {
+        let secur_key = req.body.secur_key;
+        if (secur_key === api_secur.db_demo_secur) {
+            let data_table = req.body.data_table;
+            let set = req.body.set;
+            let where = req.body.where;
+            let params = req.body.params;
+            db_demo_model.editData(data_table, set, where, params)
+                .then(result => res.json({ "mess": "ok", "result": result }))
+                .catch(err => res.json({
+                    "mess": "fail",
+                    "err": err
+                }));
+        } else {
+            res.json({
+                "mess": "fail",
+                "err": "Please privide the correct API Security key!"
+            });
+        }
+    } else {
+        res.json({
+            "mess": "fail",
+            "err": "No data post received!"
+        });
+    }
+});
+
+router.post("/delete-data", jsonParser, (req, res) => {
+    if (req.body) {
+        let secur_key = req.body.secur_key;
+        if (secur_key === api_secur.db_demo_secur) {
+            let data_table = req.body.data_table;
+            let where = req.body.where;
+            db_demo_model.deleteData(data_table, where)
+                .then(result => res.json({ "mess": "ok" }))
+                .catch(err => res.json({
+                    "mess": 'fail',
+                    "err": err
+                }));
+        } else {
+            res.json({
+                "mess": "fail",
+                "err": "Please privide the correct API Security key!"
+            });
+        }
+    } else {
+        res.json({
+            "mess": "fail",
+            "err": "No data post received!"
         });
     }
 });
